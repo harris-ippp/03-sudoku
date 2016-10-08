e4343c4b5c7189ab241652d0b6ed72be002edbf9
Jamie Saxon <jsaxon@uchicago.edu>
2016-10-08 13:33:07 -0500

    creating skeletons

---
+++ sudoku.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python 
+
+import sys
+
+class sudoku():
+
+  def __init__(self, puzzle):
+
+    self.puzzle = [int(v) for v in puzzle]
+
+    # Save a copy to check our work, later.
+
+    # Initialize the 'unassigned' lists.
+
+
+  #######
+  # CHECKS AND PRINTING
+
+  def __repr__(self):
+
+    return "".join(str(x) for x in self.puzzle) + "\n"
+
+
+  def __str__(self):
+
+    return "My great sudoku puzzle: " + str(self.puzzle)
+
+
+  def verify_solution(self): # verifying is easy!!
+  
+    print("Could be!  Let's just say yes!!")
+    return True
+        
+
+  ## THESE ARE THE MEATY METHODS THAT ACTUALLY DO THINGS...
+
+  def get_box(self, cell): 
+
+    return 0
+
+  # Get the possibilities for the row, column, and box.
+  # Their intersection is the possibilities of the cell.
+  def cell_possibilities(self, i):
+
+    pass
+
+
+  # Assign a cell to a value,
+  # and remove the possibilities from the row, column, and box.
+  def assign_cell(self, cell, value):
+
+    pass
+
+
+  # Loop over the puzzle repeatedly, 
+  # searching for cells where only a single value is possible.
+  # In those cases, assign the cell to that single value.
+  # For sets, the easiest is s.pop(); for list, it's... the first element!!
+  def assign(self):
+
+    pass
+
+
+  ## Go on... try recursion!  It's fun!!
+
+
+solved, total = 0, 0
+with open("sudoku_solved_easier.txt", "w") as out:
+  for line in open("sudoku_easier.txt"):
+
+    p = line.strip()
+    s = sudoku(p)
+    s.assign()
+
+    total += 1
+    if s.verify_solution(): solved += 1
+
+    out.write(s.__repr__())
+
+print("Easy {}/{} = {:.3f}".format(solved, total, solved/total))
+
+
